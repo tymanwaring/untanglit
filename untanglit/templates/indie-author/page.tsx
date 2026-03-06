e43dc4a90f235ea959bf591b6d73d8339ad5099e
Tyler Manwaring <uvutictac@gmail.com>
2026-03-05 18:51:40 -0800

    Reducing image size a bit more

--- untanglit/templates/indie-author/page.tsx
+++ untanglit/templates/indie-author/page.tsx
@@ -25,8 +25,8 @@ export default function IndieAuthorPage() {
               className="object-cover"
               priority
               fetchPriority="high"
-              sizes="(max-width: 1280px) 100vw, 1280px"
-              quality={80}
+              sizes="(max-width: 1024px) 100vw, 1024px"
+              quality={75}
               placeholder="blur"
               blurDataURL="data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wBDAAgGBgcGBQgHBwcJCQgKDBQNDAsLDBkSEw8UHRofHh0aHBwgJC4nICIsIxwcKDcpLDAxNDQ0Hyc5PTgyPC4zNDL/2wBDAQkJCQwLDBgNDRgyIRwhMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjL/wAARCAABAAEDASIAAhEBAxEB/8QAFQABAQAAAAAAAAAAAAAAAAAAAAv/xAAUEAEAAAAAAAAAAAAAAAAAAAAA/8QAFQEBAQAAAAAAAAAAAAAAAAAAAAX/xAAUEQEAAAAAAAAAAAAAAAAAAAAA/9oADAMBEQACEQADAPwCd/9k="
             />
